db1bab4724426ee72d25429121af6fc75397d78d
Tushar s kattimani Tushar <tusharskattimanitushar@gmail.com>
2026-03-12 09:50:22 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7358096966-b0c4c",
+  "appId": "1:466891682067:web:039f9ea3b09af3a9206ae6",
+  "apiKey": "AIzaSyAmCV-13jfhrKqVXHenIuBu0Hi7dr8919U",
+  "authDomain": "studio-7358096966-b0c4c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "466891682067"
+};
